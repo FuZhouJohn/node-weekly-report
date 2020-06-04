7e6413ea6d7a1b3333602bfc12fa0d975fcebec8
庄骥南 <a492073467@gmail.com>
2020-06-05 00:19:38 +0800

    新增 mock 的 writeFile 方法

--- __mocks__/fs.js
+++ __mocks__/fs.js
@@ -1,21 +1,34 @@
 const fs = jest.genMockFromModule('fs')
 const _fs = jest.requireActual('fs')
 
-const mocks = {}
 Object.assign(fs, _fs)
 
-fs.setMock = (path, error, data) => {
-    mocks[path] = [error, data]
+const readMocks = {}
+fs.setReadFileMock = (path, error, data) => {
+    readMocks[path] = [error, data]
 }
 
 fs.readFile = (path, options, callback) => {
     if (callback === undefined) {
         callback = options
     }
-    if (path in mocks) {
-        callback(...mocks[path])
+    if (path in readMocks) {
+        callback(...readMocks[path])
     } else {
         _fs.readFile(path, options, callback)
+    }
+}
+
+const writeMocks = {}
+fs.setWriteFileMock = (path, fn) => {
+    writeMocks[path] = fn
+}
+
+fs.writeFile = (path, data, options, callback) => {
+    if(path in writeMocks){
+        writeMocks[path](path,data,options,callback)
+    }else{
+        _fs.writeFile(path, data, options, callback)
     }
 }
 
--- __tests__/db.spec.js
+++ __tests__/db.spec.js
@@ -6,7 +6,7 @@ jest.mock('fs')
 describe('db', () => {
     it('can read', async () => {
         const data = [{title: '买水', done: false}]
-        fs.setMock('/xxx', null, JSON.stringify(data))
+        fs.setReadFileMock('/xxx', null, JSON.stringify(data))
         const list = await db.read('/xxx')
         expect(list).toStrictEqual(data)
     })
