9dd623cb622c34cd401358dd90a3cab80075298a
庄骥南 <a492073467@gmail.com>
2022-05-31 21:08:58 +0800

    改为 d.ts

--- src/index.ts
+++ src/index.ts
@@ -1,43 +1,43 @@
-import {write, read} from './db';
-import dayjs from 'dayjs';
-import {Report} from './types';
-import isoWeek from 'dayjs/plugin/isoWeek';
+import { write, read } from "./db";
+import dayjs from "dayjs";
+import isoWeek from "dayjs/plugin/isoWeek";
+import { Report } from "./types";
 
 dayjs.extend(isoWeek);
 
 export const add = async (title: string) => {
-    const list = await read();
+  const list = await read();
 
-    list.push({title, createAt: dayjs().unix()});
+  list.push({ title, createAt: dayjs().unix() });
 
-    await write(list);
+  await write(list);
 };
 
 export const clear = async () => {
-    await write([]);
+  await write([]);
 };
 
 function printAllTasks(list: Report[]) {
-    const taskWeekly = [];
-    for (let i = list.length - 1; i >= 0; i--) {
-        const task = list[i];
-        const createAt = dayjs.unix(task.createAt);
-        if (createAt.isBefore(dayjs(), 'isoWeek')) {
-            break;
-        }
-        taskWeekly.unshift(`- ${task.title}`);
+  const taskWeekly = [];
+  for (let i = list.length - 1; i >= 0; i--) {
+    const task = list[i];
+    const createAt = dayjs.unix(task.createAt);
+    if (createAt.isBefore(dayjs(), "isoWeek")) {
+      break;
     }
+    taskWeekly.unshift(`- ${task.title}`);
+  }
 
-    if (taskWeekly.length > 0) {
-        taskWeekly.forEach(text => {
-            console.log(text);
-        });
-    } else {
-        console.log('暂无记录');
-    }
+  if (taskWeekly.length > 0) {
+    taskWeekly.forEach((text) => {
+      console.log(text);
+    });
+  } else {
+    console.log("暂无记录");
+  }
 }
 
 export const showAll = async () => {
-    const list = await read();
-    printAllTasks(list);
+  const list = await read();
+  printAllTasks(list);
 };
--- src/types.ts
+++
@@ -1,4 +0,0 @@
-export interface Report {
-    title: string;
-    createAt: number;
-}
