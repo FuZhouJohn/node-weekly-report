0bebb9a5b80beb913010d7d5830afe361e5ccd3d
庄骥南 <a492073467@gmail.com>
2022-05-31 21:05:24 +0800

    link

--- build.config.ts
+++ build.config.ts
@@ -1,14 +1,12 @@
-import { defineBuildConfig } from 'unbuild'
-import fg from 'fast-glob'
+import { defineBuildConfig } from "unbuild";
+import fg from "fast-glob";
 
 export default defineBuildConfig({
-    entries: [
-        ...fg.sync('src/commands/*.ts').map(i => i.slice(0, -3)),
-    ],
-    clean: true,
-    declaration: true,
-    rollup: {
-        emitCJS: true,
-        inlineDependencies: true,
-    },
-})
+  entries: [...fg.sync("src/*.ts").map((i) => i.slice(0, -3))],
+  clean: true,
+  declaration: true,
+  rollup: {
+    emitCJS: true,
+    inlineDependencies: true,
+  },
+});
