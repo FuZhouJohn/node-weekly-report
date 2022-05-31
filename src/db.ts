188b955c97af2228e5b64750a23ba84f1508c9a0
庄骥南 <a492073467@gmail.com>
2022-05-31 22:00:47 +0800

    lint

--- src/db.ts
+++ src/db.ts
@@ -1,40 +1,35 @@
-import fs from 'fs';
-import os from 'os';
-import path from 'path';
+import fs from "fs";
+import os from "os";
+import path from "path";
+import { Report } from "./types";
 
 const homedir = os.homedir();
 const home = process.env.HOME || homedir;
-const dbPath = path.join(home, '.weekly-report');
-
-interface Report {
-    title: string;
-    createAt: number;
-}
+const dbPath = path.join(home, ".weekly-report");
 
 export const read = (path = dbPath): Promise<Report[]> => {
-    return new Promise((resolve, reject) => {
-        fs.readFile(path, {flag: 'a+'}, (error, data) => {
-            if (error) {
-                console.log(error);
-                return reject(error);
-            }
-            let list;
-            try {
-                list = JSON.parse(data.toString());
-            } catch (e) {
-                list = [];
-            }
-            resolve(list);
-        });
+  return new Promise((resolve, reject) => {
+    fs.readFile(path, { flag: "a+" }, (error, data) => {
+      if (error) {
+        console.log(error);
+        return reject(error);
+      }
+      let list;
+      try {
+        list = JSON.parse(data.toString());
+      } catch (e) {
+        list = [];
+      }
+      resolve(list);
     });
+  });
 };
-export const write = (list:Report[], path = dbPath) => {
-    return new Promise((resolve, reject) => {
-        const data = JSON.stringify(list);
-        fs.writeFile(path, data, error => {
-            if (error) return reject(error);
-            resolve('success');
-        });
+export const write = (list: Report[], path = dbPath) => {
+  return new Promise((resolve, reject) => {
+    const data = JSON.stringify(list);
+    fs.writeFile(path, data, (error) => {
+      if (error) return reject(error);
+      resolve("success");
     });
+  });
 };
-
